62a4ae2b014c6c588366d19571b754803ad40a8e
zaigiaz <shayne.tman@gmail.com>
2026-01-19 09:44:23 -0600

    changed input, added cwd function, added strcmp() for quit and help

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Overview
 This is an implementation of a shell in C, I want to support basic File and Directory commands for now on linux
 and later on do more complicated shell commands
+
+# TODO:
+TODO: add cwd command to get current directory
+TODO: use fork() to execute commands as child processes
+TODO: add command line flag parsing
--- main.c
+++ main.c
@@ -1,51 +1,80 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
 
-#define LENGTH 256
+#define LENGTH 64
 
-enum commands {
-  pwd,
-  cd,
-  ls,
-  touch,
-};
-
-void interpret_input(char* input) {}
-
-// TODO: add system calls for getting files and directories and stuff like that
-// TODO: fix extra newline being created somehow
-// TODO: 
+// TODO: execute commands as child process
+// TODO: add command line flag parsing
 
 int main() {
 
-  char *input = malloc(sizeof(char) * LENGTH);
+  char input[LENGTH];
 
+  printf("-----------------------------------");
   printf("\nWelcome to Frell: Friendly Shell!\n");
+  printf("      Commands: quit, help         \n");
   printf("-----------------------------------\n");
   
   while(true) {
 
-    printf("\n[user@shell] ");
-
-    // await input
-    scanf("%s", input);
-
-    if(*input == 'q') {
-      perror("\nquitting shell\n");
-      break;
+    char buffer[LENGTH];
+    if(getcwd(buffer, LENGTH) == NULL) {
+      perror("failed to get current directory");
+      exit(1);
     }
 
+    // print listing
+    printf("[%s ] ", buffer);
+
+
+    // read input
+    if(fgets(input,LENGTH,stdin) == NULL) {
+      perror("failed to get standard input");
+      exit(1);
+    }
+
+    // strip newline from fgets function
+    size_t length = strlen(input);
+    if(length > 0 && input[length-1] == '\n') { 
+      input[length-1] = '\0'; 
+    }
+
+    // TODO: tokenize input based on spaces
+    /* strtok(input, " "); */
+
+    
+    // quit action
+    if(strcmp(input, "quit") == 0) {
+      perror("\nquitting shell\n");
+      exit(1);
+    }
+
+    // help action
+    if(strcmp(input, "help") == 0) {
+      printf("\nCurrent shell works with basic UNIX commands, through execvp() function\n");
+    }
+
+
     // interpret input
-    interpret_input(input);
+    /* pid_t pid = fork(); */
+    /* if(pid == 0) { */
+    /*   execvp(args[0], args); */
+    /*   perror("exec failed"); */
+    /*   exit(1); */
+    /* } */
+    /* else if (pid > 1) { */
+      
+    /* } */
+
 
     // flush buffer
     size_t len = sizeof(input);
     memset(input, 0x00, len);
   }
 
-  free(input);
 
   return 0;
 }
