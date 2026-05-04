cb7d106fe24edde42b328fe46483efe0d3480e25
zaigiaz <shayne.tman@gmail.com>
2026-05-04 10:32:08 -0500

    added main fork loop, can call commands in shell now

--- main.c
+++ main.c
@@ -3,18 +3,20 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <signal.h>
 
 #define LENGTH 256
 #define SIG_NO_CUR_DIR 1
 
-enum shell_actions {QUIT, HELP};
+enum shell_actions { QUIT, HELP };
 
 void other_actions(const char *input);
+bool if_background(const char* input);
 enum shell_actions StringtoEnum(const char* str);
 
-// TODO :: use strtok() method to split flags and commands
-// TODO :: use execvp to handle things
+// TODO :: make main fork loop into seperate function / structure
 // TODO :: implement signals from gist to handle diff program outputs
 // TODO :: implement jobs and job handling
 
@@ -52,17 +54,64 @@ int main() {
       input[length-1] = '\0'; 
     }
     
-    //actions you can take
+    // actions you can take (util functions for shell)
     other_actions(input);
 
-    // flush buffer
-    size_t len = sizeof(input);
+    // check to see if background process
+    bool bg = if_background(input);
+
+    // fork the process and tokenize for arguments
+    pid_t pid = fork();
+    if (pid == 0) {
+      char *args[64];
+      size_t arg_count = 0;
+      char *token = strtok(input, " ");
+
+      // get each argument for the command in input
+      while (token != NULL && arg_count < 63) {
+	args[arg_count++] = token;
+	token = strtok(NULL, " ");
+      }
+      
+      // null-terminate array
+      args[arg_count] = NULL;
+
+      execvp(args[0], args);
+      perror("exec failed");
+      exit(1);
+    } 
     
+    // else if we are parent_process
+    else if (pid > 0) {
+      int status;
+      if (!bg) {
+        waitpid(pid, &status, 0);
+        printf(WIFEXITED(status) ? "Success\n" : "Failure\n");
+      } else {
+	printf("child started in background");
+      }
+    }
+
+    else {
+      perror("fork failed");
+    }
+
+    // flush buffer and reset
+    size_t len = sizeof(input);
     memset(input, 0x00, len);
   }
 
-
   return 0;
+}
+
+bool if_background(const char* input) {
+  if (!input || *input == '\0') return false;
+
+  if (input[strlen(input) - 1] == '&') {
+    return true;
+  }
+
+  return false;
 }
 
 // Take shell input and convert to a command
@@ -79,6 +128,7 @@ void other_actions(const char* input) {
     break;
   }
 }
+
 
 // Take a string from user-input and convert to int
 enum shell_actions StringtoEnum(const char* str) {
