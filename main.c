2b838a2fa82136ace234aa26555053a2e167b9d7
zaigiaz <shayne.tman@gmail.com>
2026-05-14 09:03:14 -0500

    working on piping

--- main.c
+++ main.c
@@ -30,6 +30,8 @@ void start_sig_handling();
 void sigchild_handler(int signum);
 void sigint_handler(int sig);
 void sigtstp_handler(int sig);
+void piping(const char* p1, const char* p2);
+
 
 Job jobs[MAX_JOBS];
 size_t job_count = 0;
@@ -173,7 +175,7 @@ void other_actions(const char* input) {
   switch (inputAction) {
   case QUIT: perror("\nquitting shell\n"); exit(0);
     break;
-  case HELP: printf("\nCurrent shell works with basic UNIX commands, through execvp() function\n");
+  case HELP: printf("\nCurrent shell works with basic UNIX commands, through execvp() function, no piping.\n");
     break;
   default: 
     break;
@@ -251,3 +253,53 @@ void sigint_handler(int sig) {
 void sigtstp_handler(int sig) {
   waitpid(fg_pid, &sig, 0);
 }
+
+// if the second argument is '|' then we take arg[0] and arg[1] and do basic pipe
+// NOTE :: doesnt support multi-piping
+void piping(const char* p1, const char* p2) {
+  int pipefd[2];
+  pid_t cpid;
+  char buf;
+
+ if (pipe(pipefd) == -1)          /* An error has occurred. */
+  {
+   fprintf(stderr, "%s", "The call to pipe() has failed.\n");           
+   exit(EXIT_FAILURE);
+  }
+
+ cpid = fork();
+ 
+ if (cpid == -1)                  /* An error has occurred. */
+  {
+   fprintf(stderr, "%s", "The call to fork() has failed.\n");
+   exit(EXIT_FAILURE);
+  }
+
+ // if child process
+ if (cpid == 0) 
+  {
+   close(pipefd[1]);              /* Close unused write end */
+   printf("The child is about to read from the pipe.\n");
+   while (read(pipefd[0], &buf, 1) > 0)
+     write(STDOUT_FILENO, &buf, 1);
+   write(STDOUT_FILENO, "\n", 1);
+   close(pipefd[0]);
+   printf("The child has just echoed from the pipe to standard output.\n");
+   _exit(EXIT_SUCCESS);
+  } 
+
+ else 
+  {                               /* Parent writes argv[1] to pipe */
+   printf("I am the parent.\n");
+   close(pipefd[0]);              /* Close unused read end */
+   write(pipefd[1], p1, strlen(p1));
+   close(pipefd[1]);              /* Closing creates the EOF marker. */
+   printf("The parent has just written data into the pipe.\n");
+   printf("The parent will now wait for the child to terminate.\n");
+   wait(NULL);                    /* Parent waits for the child to terminate */
+   exit(EXIT_SUCCESS);
+  }
+
+  return;
+}
+
